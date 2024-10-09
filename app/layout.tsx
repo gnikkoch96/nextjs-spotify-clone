e886905157292661f124359edb5137a23d877397
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2024-10-08 20:07:46 -0700

    add library and box components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import {Figtree} from "next/font/google";
 import "./globals.css";
+import Sidebar from "@/components/Sidebar";
 
 const font = Figtree({subsets: ['latin']});
 
@@ -19,7 +20,9 @@ export default function RootLayout({
       <body
         className={font.className}
       >
-        {children}
+        <Sidebar>
+            {children}
+        </Sidebar>
       </body>
     </html>
   );
